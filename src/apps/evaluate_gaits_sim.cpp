9584539bdbdff191644363177f71811ab6eec56d
David-Estevez <david.estevez.fdez@gmail.com>
2014-06-19 20:42:43 +0200

    Restored time tracking.

--- src/apps/evaluate_gaits_sim.cpp
+++ src/apps/evaluate_gaits_sim.cpp
@@ -73,9 +73,9 @@ int main(int argc, char * argv[] )
     std::cout << "Evaluate-Gaits (simulated version)" << std::endl
               << "------------------------------------" << std::endl;
 
-//    //-- Time track:
-//    struct timeval starttime, endtime;
-//    gettimeofday( &starttime, NULL);
+    //-- Time track:
+    struct timeval starttime, endtime;
+    gettimeofday( &starttime, NULL);
 
     //-- Run the robot:
     std::cout << "[Evaluate] Run!" << std::endl;
@@ -97,22 +97,22 @@ int main(int argc, char * argv[] )
     }
 
 
-//    //-- Get actual elapsed time:
-//    gettimeofday(&endtime, NULL);
+    //-- Get actual elapsed time:
+    gettimeofday(&endtime, NULL);
 
-//    int sec_diff = endtime.tv_sec-starttime.tv_sec;
-//    int usec_diff = endtime.tv_usec-starttime.tv_usec;
-//    if (usec_diff < 0)
-//    {
-//        usec_diff += 1000000;
-//        sec_diff -= 1;
-//    }
+    int sec_diff = endtime.tv_sec-starttime.tv_sec;
+    int usec_diff = endtime.tv_usec-starttime.tv_usec;
+    if (usec_diff < 0)
+    {
+        usec_diff += 1000000;
+        sec_diff -= 1;
+    }
 
     //-- Report distance travelled:
     std::cout << "Distance travelled: " << robotInterface->getTravelledDistance() << std::endl;
-//    std::cout << "Robot time elapsed: " << (int) run_time << std::endl;
-//    std::cout << "Real time elapsed: " << sec_diff << "s " << usec_diff << "us " << std::endl;
-//    std::cout << std::endl;
+    std::cout << "Robot time elapsed: " << (int) run_time << std::endl;
+    std::cout << "Real time elapsed: " << sec_diff << "s " << usec_diff << "us " << std::endl;
+    std::cout << std::endl;
 
     std::cout << "Finished!" << std::endl;
 
